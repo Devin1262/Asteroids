675d34bd0b99c24fba98273525cdb7565a4e576a
Devin1262 <devintran58@gmail.com>
2024-12-02 20:25:35 -0500

    Added the player and movement for it

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
-
-pygame.init()
+from player import *
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2 )
     while(True):
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+        pygame.display.flip()
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
     
     
 if __name__ == "__main__":
